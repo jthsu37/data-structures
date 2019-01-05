fde1ac17477fca6652c65573366d7e6912b9c312
john hsu <hsu@johns-Air.home>
2019-01-04 19:36:23 -0800

    finished stack prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,7 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = Object.create(Stack.prototype);
+  var someInstance = Object.create(stackMethods);
   someInstance.sizeCount = 0;
   someInstance.keyCounter = 0;
 
@@ -10,21 +10,22 @@ var Stack = function() {
 
 var stackMethods = {};
 
-
-Stack.prototype.push = function(value) {
+stackMethods.push = function(value) {
   var temp = this.keyCounter;
   this[temp] = value;
   this.keyCounter++;
   this.sizeCount++;
 };
-Stack.prototype.pop = function() {
+
+stackMethods.pop = function() {
   var tempKeys = Object.keys(this);
   var poppedValue = this[tempKeys[tempKeys.length - 3]];
   delete this[tempKeys[tempKeys.length - 3]];
   this.sizeCount--;
   return poppedValue;
 };
-Stack.prototype.size = function() {
+
+stackMethods.size = function() {
   if (this.sizeCount < 0) {
     return 0;
   } else {
