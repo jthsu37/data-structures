61d91cdfc4dd80df36cb7ccc87ec5f6962e7bbdb
john hsu <hsu@johns-Air.home>
2019-01-05 11:36:38 -0800

    finished prototypal queue, sizeCount cannot be lessthan 0 now

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.sizeCount = 0;
+  someInstance.keyCount = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  var temp = this.keyCount;
+  this[temp] = value;
+  this.sizeCount++;
+  this.keyCount++;
+};
 
+queueMethods.dequeue = function() {
+  var tempKeys = Object.keys(this);
+  if (this.sizeCount > 0) {
+    this.sizeCount--;
+  }
+  if (tempKeys[0] !== 'sizeCount') {
+    var shiftValue = this[tempKeys[0]];
+    delete this[tempKeys[0]];
+    return shiftValue;
+  }
+};
+
+queueMethods.size = function() {
+  return this.sizeCount;
+};
