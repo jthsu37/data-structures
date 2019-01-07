8df2c999934dc70e7de332eca702c4c8a25018cf
john hsu <hsu@johns-Air.home>
2019-01-07 15:19:20 -0800

    finished sprint two set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this[item] !== item.toString()) {
+    this[item] = item.toString();
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this) {
+    if (key === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add, contains is O[n]
+ remove is O[1]
  */
