cf5685831bb5bcda5244d550c9f7989a158543a5
john hsu <hsu@johns-Air.home>
2019-01-07 15:37:41 -0800

    finished complexity for hashTableHelpers

--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -56,4 +56,6 @@ var getIndexBelowMaxForKey = function(str, max) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+.checkLimit, .get, .set = O(1)
+.each, getIndexBelowMaxForKey = O(n)
  */
