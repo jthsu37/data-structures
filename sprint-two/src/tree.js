7b4f9cec14998bed72f3e3100992cc93a40813d1
john hsu <hsu@johns-Air.home>
2019-01-08 15:09:11 -0800

    redid tree contains with recursion

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -14,19 +14,20 @@ var treeMethods = {};
 treeMethods.addChild = function(value) {
   this.children.push({value: value, children: [], addChild: this.addChild, contains: this.contains});
 };
-
 treeMethods.contains = function(target) {
-  var stringTree = JSON.stringify(this);
-
-  for (var i = 0; i < stringTree.length; i++) {
-    if (target.toString() === stringTree[i]) {
-      return true;
+  var found = false;
+  var recursion = function(target, tree) {
+    if (tree.value === target) {
+      found = true;
+    } else if (tree.children) {
+      for (var i = 0; i < tree.children.length; i++) {
+        recursion(target, tree.children[i]);
+      }
     }
   }
-  return false;
+  recursion(target, this);
+  return found;
 };
-
-
 
 /*
  Complexity: What is the time complexity of the above functions?
