2c51ce70ae5570828f9b569fb77b8a5f0ff8ea54
john hsu <hsu@johns-Air.home>
2019-01-08 19:14:19 -0800

    uncommented one test and refactored hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,8 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.counter = 0;
+  this.storage = [];
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -9,8 +11,26 @@ HashTable.prototype.insert = function(k, v) {
 
   if (this._storage.get(index) && value[0] !== k) {
     this._storage.set(index, [value, [k, v]]);
+    this.counter++;
   } else {
     this._storage.set(index, [k, v]);
+    this.counter++;
+  }
+
+  if (Math.floor(this._limit * .75) === this.counter){
+    for (var i = 0; i < this._limit; i++) {
+      var tuple = this._storage.get(i);
+      this.storage.push(tuple);
+    }
+
+    this._limit = this._limit * 2;
+    this._storage = LimitedArray(this._limit);
+
+    for (var j = 0; j < this._limit; j++) {
+      if (this.storage[j] !== undefined) {
+        this._storage.set(j, [this.storage[j][0], this.storage[j][1]]);
+      }
+    }
   }
 };
 
@@ -38,10 +58,12 @@ HashTable.prototype.remove = function(k) {
 
   if (typeof value[0] === 'string') {
     this._storage.set(index);
+    this.counter--;
   } else {
     for (var i = 0; i < value.length; i++) {
       if (value[i][0] === k) {
         this._storage.set(index)
+        this.counter--;
       }
     }
   }
